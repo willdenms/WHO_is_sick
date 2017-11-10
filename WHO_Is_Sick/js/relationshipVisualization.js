6e56579ddac19b6968eb72af44cc0b71735baa46
Naoki Tominaga <u0876779@utah.edu>
2017-11-10 12:28:31 -0700

    getting nodes to collapse but links not showing up

--- WHO_Is_Sick/js/relationshipVisualization.js
+++ WHO_Is_Sick/js/relationshipVisualization.js
@@ -7,7 +7,8 @@ class relationshipVisualization {
 
         var width = +svg.attr("width"),
             height = +svg.attr("height"),
-            g = svg.append("g").attr("transform", "translate(" + (width / 3) + "," + (height / 2.5) + ")");
+            g = svg.append("g").attr("transform", "translate(" + (width / 3) + "," + (height / 2.5) + ")"),
+            root;
         //svg.append("g").attr("transform", "translate(" + (width / 2 + 40) + "," + (height / 2 + 90) + ")");
 
         var stratify = d3.stratify()
@@ -24,8 +25,7 @@ class relationshipVisualization {
         d3.csv("data/fakeData.csv", function (error, data) {
             if (error) throw error;
 
-            var root = tree(stratify(data));
-            console.log(root);
+            root = tree(stratify(data));
 
             function collapse(d) {
                 if (d.children) {
@@ -35,7 +35,7 @@ class relationshipVisualization {
                 }
             }
 
-            root.children.forEach(collapse);
+            root.children.forEach(collapse); // this line collapses tree to root and its children
             update(root);
         });
 
@@ -44,31 +44,90 @@ class relationshipVisualization {
         }
 
         function update(source){
+            var treeData = tree(root);
 
 
-            var link = g.selectAll(".link")
-                .data(source.links())
-                .enter().append("path")
-                .attr("class", "link")
-                .attr("d", d3.linkRadial()
-                    .angle(function(d) { return d.x; })
-                    .radius(function(d) { return d.y; }));
+            let nodes = treeData.descendants(),
+                links = treeData.descendants().slice(1);
 
             var node = g.selectAll(".node")
-                .data(source.descendants())
-                .enter().append("g")
-                .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
-                .attr("transform", function(d) { return "translate(" + radialPoint(d.x, d.y) + ")"; });
+                .data(nodes);
 
-            node.append("circle")
+            var nodeEnter = node.enter().append("g")
+                .attr("class", function(d) { console.log("enter");return "node" + (d.children ? " node--internal" : " node--leaf"); })
+                .attr("transform", function(d) { return "translate(" + radialPoint(d.x, d.y) + ")"; })
+                .on("click", click);
+
+            nodeEnter.append("circle")
                 .attr("r", 5.5);
 
-            node.append("text")
+            nodeEnter.append("text")
                 .attr("dy", "0.31em")
                 .attr("x", function(d) { return d.x < Math.PI === !d.children ? 6 : -6; })
                 .attr("text-anchor", function(d) { return d.x < Math.PI === !d.children ? "start" : "end"; })
                 .attr("transform", function(d) { return "rotate(" + (d.x < Math.PI ? d.x - Math.PI / 2 : d.x + Math.PI / 2) * 180 / Math.PI + ")"; })
                 .text(function(d) { return d.id.substring(d.id.lastIndexOf(".") + 1); });
+
+            var nodeUpdate = nodeEnter.merge(node);
+
+            nodeUpdate
+                .transition()
+                .duration(750)
+                .attr("transform", function(d) { return "translate(" + radialPoint(d.x, d.y) + ")"; });
+
+            nodeUpdate.select("circle.node")
+                .attr("r", 5.5)
+                .attr("class", function(d) { console.log("update");return "node" + (d.children ? " node--internal" : " node--leaf"); });
+
+            var nodeExit = node.exit().transition()
+                .duration(750)
+                .attr("transform", function(d) { console.log("remove"); return "translate(" + radialPoint(d.x, d.y) + ")"; })
+                .remove();
+
+            nodeExit.select('circle')
+                .attr('r', 1e-6);
+
+            nodeExit.select('text')
+                .style('fill-opacity', 1e-6);
+
+
+
+            var link = g.selectAll(".link")
+                .data(links);
+
+            var linkEnter = link.enter().append("path")
+                .attr("class", "link")
+                .attr("d", d3.linkRadial()
+                    .angle(function(d) { return d.x; })
+                    .radius(function(d) { return d.y; }));
+
+            var linkUpdate = linkEnter.merge(link);
+
+            linkUpdate.transition()
+                .duration(750)
+                .attr("d", d3.linkRadial()
+                    .angle(function(d) { return d.x; })
+                    .radius(function(d) { return d.y; }));
+
+            var linkExit = link.exit().transition()
+                .duration(750)
+                .attr("d", d3.linkRadial()
+                    .angle(function(d) { return d.x; })
+                    .radius(function(d) { return d.y; }))
+                .remove();
+
+            function click(d) {
+                if (d.children) {
+                    d._children = d.children;
+                    d.children = null;
+                } else {
+                    d.children = d._children;
+                    d._children = null;
+                }
+                update(d);
+            }
+
+
         }
     }
 
