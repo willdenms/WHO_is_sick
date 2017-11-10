a45f99087d5135ccaae6612381b46db0ff5f524f
Matt Willden <willdenms@gmail.com>
2017-11-10 16:53:15 -0700

    The globe is working, working on linking real data

--- WHO_Is_Sick/js/script.js
+++ WHO_Is_Sick/js/script.js
@@ -1,5 +1,6 @@
 // This is the script.js file
 let graph = new relationshipVisualization();
+let globalMap = new GlobalMap();
 
 d3.csv("data/barChart.csv", function(error, statistics){
     let barChart = new BarChart(statistics);
