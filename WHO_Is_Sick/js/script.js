cb4d63469820f50e7ae9e2b2be3169a21c6244dc
Paarth Lakhani <paarth.lakhani@utah.edu>
2017-11-10 19:34:45 -0700

    Bar Chart has been created. Currently working on improvising it

--- WHO_Is_Sick/js/script.js
+++ WHO_Is_Sick/js/script.js
@@ -3,4 +3,5 @@ let graph = new relationshipVisualization();
 
 d3.csv("data/barChart.csv", function(error, statistics){
     let barChart = new BarChart(statistics);
+    barChart.createTable();
 });
