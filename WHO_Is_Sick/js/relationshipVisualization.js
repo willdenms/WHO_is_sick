46170069195a56d6df810634c7189a240fdd2d41
Naoki Tominaga <u0876779@utah.edu>
2017-11-10 19:25:59 -0700

    reverted tree to non-collapsible

--- WHO_Is_Sick/js/relationshipVisualization.js
+++ WHO_Is_Sick/js/relationshipVisualization.js
@@ -27,15 +27,15 @@ class relationshipVisualization {
 
             root = tree(stratify(data));
 
-            function collapse(d) {
-                if (d.children) {
-                    d._children = d.children;
-                    d._children.forEach(collapse);
-                    d.children = null;
-                }
-            }
+            // function collapse(d) {
+            //     if (d.children) {
+            //         d._children = d.children;
+            //         d._children.forEach(collapse);
+            //         d.children = null;
+            //     }
+            // }
 
-            root.children.forEach(collapse); // this line collapses tree to root and its children
+            //root.children.forEach(collapse); // this line collapses tree to root and its children
             update(root);
         });
 
@@ -44,88 +44,87 @@ class relationshipVisualization {
         }
 
         function update(source){
-            var treeData = tree(root);
+            // All commented out code is for collapsing the tree which is not fully implemented yet
+            // var treeData = tree(root);
 
 
-            let nodes = treeData.descendants(),
-                links = treeData.descendants().slice(1);
+            // let nodes = treeData.descendants(),
+            //     links = treeData.descendants().slice(1);
 
             var node = g.selectAll(".node")
-                .data(nodes);
-
-            var nodeEnter = node.enter().append("g")
+                .data(source.descendants())
+                .enter().append("g")
                 .attr("class", function(d) { console.log("enter");return "node" + (d.children ? " node--internal" : " node--leaf"); })
-                .attr("transform", function(d) { return "translate(" + radialPoint(d.x, d.y) + ")"; })
-                .on("click", click);
+                .attr("transform", function(d) { return "translate(" + radialPoint(d.x, d.y) + ")"; });
+                //.on("click", click);
 
-            nodeEnter.append("circle")
+            node.append("circle")
                 .attr("r", 5.5);
 
-            nodeEnter.append("text")
+            node.append("text")
                 .attr("dy", "0.31em")
                 .attr("x", function(d) { return d.x < Math.PI === !d.children ? 6 : -6; })
                 .attr("text-anchor", function(d) { return d.x < Math.PI === !d.children ? "start" : "end"; })
                 .attr("transform", function(d) { return "rotate(" + (d.x < Math.PI ? d.x - Math.PI / 2 : d.x + Math.PI / 2) * 180 / Math.PI + ")"; })
                 .text(function(d) { return d.id.substring(d.id.lastIndexOf(".") + 1); });
 
-            var nodeUpdate = nodeEnter.merge(node);
-
-            nodeUpdate
-                .transition()
-                .duration(750)
-                .attr("transform", function(d) { return "translate(" + radialPoint(d.x, d.y) + ")"; });
-
-            nodeUpdate.select("circle.node")
-                .attr("r", 5.5)
-                .attr("class", function(d) { console.log("update");return "node" + (d.children ? " node--internal" : " node--leaf"); });
-
-            var nodeExit = node.exit().transition()
-                .duration(750)
-                .attr("transform", function(d) { console.log("remove"); return "translate(" + radialPoint(d.x, d.y) + ")"; })
-                .remove();
-
-            nodeExit.select('circle')
-                .attr('r', 1e-6);
-
-            nodeExit.select('text')
-                .style('fill-opacity', 1e-6);
+            // var nodeUpdate = nodeEnter.merge(node);
+            //
+            // nodeUpdate
+            //     .transition()
+            //     .duration(750)
+            //     .attr("transform", function(d) { return "translate(" + radialPoint(d.x, d.y) + ")"; });
+            //
+            // nodeUpdate.select("circle.node")
+            //     .attr("r", 5.5)
+            //     .attr("class", function(d) { console.log("update");return "node" + (d.children ? " node--internal" : " node--leaf"); });
+            //
+            // var nodeExit = node.exit().transition()
+            //     .duration(750)
+            //     .attr("transform", function(d) { console.log("remove"); return "translate(" + radialPoint(d.x, d.y) + ")"; })
+            //     .remove();
+            //
+            // nodeExit.select('circle')
+            //     .attr('r', 1e-6);
+            //
+            // nodeExit.select('text')
+            //     .style('fill-opacity', 1e-6);
 
 
 
             var link = g.selectAll(".link")
-                .data(root.links());
-
-            var linkEnter = link.enter().append("path")
+                .data(source.links())
+                .enter().append("path")
                 .attr("class", "link")
                 .attr("d", d3.linkRadial()
                     .angle(function(d) { return d.x; })
                     .radius(function(d) { return d.y; }));
 
-            var linkUpdate = linkEnter.merge(link);
-
-            linkUpdate.transition()
-                .duration(750)
-                .attr("d", d3.linkRadial()
-                    .angle(function(d) { return d.x; })
-                    .radius(function(d) { return d.y; }));
-
-            var linkExit = link.exit().transition()
-                .duration(750)
-                .attr("d", d3.linkRadial()
-                    .angle(function(d) { return d.x; })
-                    .radius(function(d) { return d.y; }))
-                .remove();
-
-            function click(d) {
-                if (d.children) {
-                    d._children = d.children;
-                    d.children = null;
-                } else {
-                    d.children = d._children;
-                    d._children = null;
-                }
-                update(d);
-            }
+            // var linkUpdate = linkEnter.merge(link);
+            //
+            // linkUpdate.transition()
+            //     .duration(750)
+            //     .attr("d", d3.linkRadial()
+            //         .angle(function(d) { return d.x; })
+            //         .radius(function(d) { return d.y; }));
+            //
+            // var linkExit = link.exit().transition()
+            //     .duration(750)
+            //     .attr("d", d3.linkRadial()
+            //         .angle(function(d) { return d.x; })
+            //         .radius(function(d) { return d.y; }))
+            //     .remove();
+            //
+            // function click(d) {
+            //     if (d.children) {
+            //         d._children = d.children;
+            //         d.children = null;
+            //     } else {
+            //         d.children = d._children;
+            //         d._children = null;
+            //     }
+            //     update(d);
+            // }
 
 
         }
