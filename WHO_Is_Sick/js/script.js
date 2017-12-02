3f771d8dc66c4d9a5983cfcb7d869f370bb78c4c
Paarth Lakhani <paarth.lakhani@utah.edu>
2017-12-01 20:32:03 -0700

    Made changes to use the JSON file

--- WHO_Is_Sick/js/script.js
+++ WHO_Is_Sick/js/script.js
@@ -2,20 +2,15 @@
 
 let globalMap = new GlobalMap();
 
-d3.csv("data/WHO_stats_2015_5.csv", function(error, dataCSV){
+d3.json("data/dummyDataStructureProper.json", function(error, jsonFile){
     // 0.1 -- 0.1*1000 = 100; 100 people died of the total people died in that country
-    let barChart;
-
-    d3.csv("data/WHO_stats_2015_5_transpose.csv", function(error, dataCSV_transpose){
-        barChart = new BarChart(dataCSV, dataCSV_transpose);
-        barChart.createTableReal();
-    });
+    let barChart = new BarChart(jsonFile);
+    barChart.createTableReal(jsonFile);
 
     let graph = new relationshipVisualization(barChart);
 
     d3.csv("data/diesease_tree.csv", function (error, data) {
         if (error) throw error;
-
         graph.createTree(data);
     });
 });
