981b43940c0e55e6c26563ddeb7e02fe95e97e74
Matt Willden <willdenms@gmail.com>
2017-12-02 21:06:04 -0700

    Everything is working, merge worked

--- WHO_Is_Sick/js/choropleth.js
+++ WHO_Is_Sick/js/choropleth.js
@@ -5,119 +5,135 @@ class Choropleth {
     }
 
     drawMap(disease) {
-
         //clear current map
-        var selection = d3.select("#globalMap").selectAll("svg").remove();
 
-        let format = d3.format(",");
-
-// Set tooltips
-        let tip = d3.tip()
-            .attr('class', 'd3-tip')
-            .offset([-10, 0])
-            .html(function (d) {
-                return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Population: </strong><span class='details'>" + format(d.population) + "</span>";
-            })
-
-        let margin = {top: -100, right: 0, bottom: 0, left: 0},
-            width = 1000 - margin.left - margin.right,
-            height = 600 - margin.top - margin.bottom;
-
-        let color = d3.scaleThreshold()
-            .domain([10000, 100000, 500000, 1000000, 5000000, 10000000, 50000000, 100000000, 500000000, 1500000000])
-            .range(["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)", "rgb(33,113,181)", "rgb(8,81,156)", "rgb(8,48,107)", "rgb(3,19,43)"]);
-
-        var path = d3.geoPath();
-
-
-        let headerSelection = d3.select("#selection").select("h").remove();
-        headerSelection = d3.select("#selection").append("h").text(disease);
-
-
-        let svg = d3.select("#globalMap")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .append('g')
-            .attr('class', 'map');
-
-        let projection = d3.geoMercator()
-            .scale(130)
-            .translate([width / 2, height / 1.5]);
-
-        path = d3.geoPath().projection(projection);
-
-        svg.call(tip);
 
         queue()
             .defer(d3.json, "data/world_countries.json")
-            .defer(d3.tsv, "data/stubData.tsv")
-            // .defer(d3.tsv, "data/WHO_stats_2015_5_transpose.tsv")
+            // .defer(d3.tsv, "data/stubData.tsv")
             .await(ready);
 
-        function ready(error, data, population) {
+        function ready(error, data) {
+            d3.csv("data/WHO_stats_2015_5_transpose.csv", function (error, whoData) {
 
 
-            let populationById = {};
 
-            // console.log("this is the dataCSV: " + self.data);
-            population.forEach(function (d) {
+                var selection = d3.select("#globalMap").selectAll("svg").remove();
 
-                populationById[d.id] = +d.population;;
-                // console.log("this is my new breakpoint: " + populationById[d.id]);
+                let format = d3.format(",");
 
-            });
-            data.features.forEach(function (d) {
-                d.CountryName = populationById[d.id]
-            });
+// Set tooltips
+                let tip = d3.tip()
+                    .attr('class', 'd3-tip')
+                    .offset([-10, 0])
+                    .html(function (d) {
+                        return "<strong>Country: </strong><span class='details'>" + d.properties.name + "<br></span>" + "<strong>Mortality: </strong><span class='details'>" + format(d.disease) + "</span>";
+                    })
 
-            svg.append("g")
-                .attr("class", "countries")
-                .selectAll("path")
-                .data(data.features)
-                .enter().append("path")
-                .attr("d", path)
-                .style("fill", function (d) {
-                    return color(populationById[d.id]);
-                })
-                .style('stroke', 'white')
-                .style('stroke-width', 1.5)
-                .style("opacity", 0.8)
-                // tooltips
-                .style("stroke", "white")
-                .style('stroke-width', 0.3)
-                .on('mouseover', function (d) {
-                    tip.show(d);
+                let margin = {top: -100, right: 0, bottom: 0, left: 0},
+                    width = 1000 - margin.left - margin.right,
+                    height = 600 - margin.top - margin.bottom;
 
-                    d3.select(this)
-                        .style("opacity", 1)
-                        .style("stroke", "white")
-                        .style("stroke-width", 3);
-                })
-                .on('mouseout', function (d) {
-                    tip.hide(d);
+                let colors = ["#ffe6e6", "#ffcccc","#ffb3b3","#ff8080", "#ff4d4d","#ff1a1a","#e60000","#b30000","#800000"];
 
-                    d3.select(this)
-                        .style("opacity", 0.8)
-                        .style("stroke", "white")
-                        .style("stroke-width", 0.3);
+                let color = d3.scaleThreshold()
+                // .domain([10000,100000,500000,1000000,5000000,10000000,50000000,100000000,500000000,1500000000])
+                    .domain([1, 50, 100, 200, 400, 800, 1000, 1500, 1800, 2000])
+                    // .range(["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)", "rgb(33,113,181)", "rgb(8,81,156)", "rgb(8,48,107)", "rgb(3,19,43)"]);
+                    .range(colors);
+
+                var path = d3.geoPath();
+
+
+                let headerSelection = d3.select("#selection").select("h").remove();
+                headerSelection = d3.select("#selection").append("h").text(disease);
+
+
+                let svg = d3.select("#globalMap")
+                    .append("svg")
+                    .attr("width", width)
+                    .attr("height", height)
+                    .append('g')
+                    .attr('class', 'map');
+
+                let projection = d3.geoMercator()
+                    .scale(130)
+                    .translate([width / 2, height / 1.5]);
+
+                path = d3.geoPath().projection(projection);
+
+                svg.call(tip);
+
+                // console.log("this should be the disease " + disease);
+
+                let diseaseByCountryName = {};
+
+                // console.log("this is the dataCSV: " + self.data);
+
+                //WHO data parsing
+                whoData.forEach(function (d) {
+
+                    diseaseByCountryName[d.CountryCode] = +d[disease];
+                    // console.log("this is my new breakpoint: " + diseaseByCountryName[d.id]);
+
                 });
 
-            svg.append("path")
-                .datum(topojson.mesh(data.features, function (a, b) {
-                    return a.id !== b.id;
-                }))
-                // .datum(topojson.mesh(data.features, function(a, b) { return a !== b; }))
-                .attr("class", "names")
-                .attr("d", path);
+                //Json world countries parsing
+                data.features.forEach(function (d) {
+
+                    d.disease = diseaseByCountryName[d.id]
+                });
+
+                svg.append("g")
+                    .attr("class", "countries")
+                    .selectAll("path")
+                    .data(data.features)
+                    .enter().append("path")
+                    .attr("d", path)
+                    .style("fill", function (d) {
+                        console.log(diseaseByCountryName[d.id]);
+                        return color(diseaseByCountryName[d.id]);
+                    })
+                    .style('stroke', 'white')
+                    .style('stroke-width', 1.5)
+                    .style("opacity", 0.8)
+                    // tooltips
+                    .style("stroke", "white")
+                    .style('stroke-width', 0.3)
+                    .on('mouseover', function (d) {
+                        tip.show(d);
+
+                        d3.select(this)
+                            .style("opacity", 1)
+                            .style("stroke", "white")
+                            .style("stroke-width", 3);
+                    })
+                    .on('mouseout', function (d) {
+                        tip.hide(d);
+
+                        d3.select(this)
+                            .style("opacity", 0.8)
+                            .style("stroke", "white")
+                            .style("stroke-width", 0.3);
+                    });
+
+                svg.append("path")
+                    .datum(topojson.mesh(data.features, function (a, b) {
+                        return a.id !== b.id;
+                    }))
+                    // .datum(topojson.mesh(data.features, function(a, b) { return a !== b; }))
+                    .attr("class", "names")
+                    .attr("d", path);
+
+
+            });
         }
+
     }
 
-    updateMap(diseaseName) {
+    updateMap(diseaseName){
         this.drawMap(diseaseName);
-        console.log('a new Disease is selected from graph' + diseaseName);
-
-
+        // console.log('a new Disease is selected from graph' + diseaseName);
 
     }
 
